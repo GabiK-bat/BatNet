7326a9509a88992c73fd0be6dacace702a60f9aa
alexander-g <3867427+alexander-g@users.noreply.github.com>
2023-04-11 07:43:28 +0000

    settings option to flag not-a-bats

--- frontend/bats/settings.js
+++ frontend/bats/settings.js
@@ -5,7 +5,10 @@ BatSettings = class extends BaseSettings{
         super.apply_settings_from_modal()
         
         GLOBAL.settings['confidence_threshold'] = Number($("#settings-confidence-threshold-input")[0].value);
+        GLOBAL.settings['flag_negatives']       = $("#settings-flag-negatives").checkbox('is checked');
         GLOBAL.settings['export_boxes']         = $("#settings-export-boxes").checkbox('is checked');
+
+        GLOBAL.App.Detection.update_all_flags()
     }
 
     //override
@@ -15,6 +18,7 @@ BatSettings = class extends BaseSettings{
         const settings = GLOBAL.settings;
 
         $("#settings-confidence-threshold-input")[0].value = settings.confidence_threshold
+        $("#settings-flag-negatives").checkbox(settings.flag_negatives? 'check' : 'uncheck');
         $("#settings-export-boxes").checkbox(settings.export_boxes? 'check' : 'uncheck');
     }
 
